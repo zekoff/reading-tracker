28a75f0124321f1ca49d2ddc1c56cd5cdf987f84
Michael Zekoff <zekoff@gmail.com>
2019-08-17 18:31:02 -0500

    Add form to page

--- readingtracker.py
+++ readingtracker.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import dbhelper
 
 application = Flask(__name__)  # gunicorn looks for 'application'
@@ -7,3 +7,7 @@ application = Flask(__name__)  # gunicorn looks for 'application'
 @application.route('/')
 def index():
     return render_template('index.html', passage=dbhelper.get_next_reading())
+
+@application.route('/record_reading')
+def record_reading():
+    return request.form['time_spent']
