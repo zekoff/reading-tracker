e31a05541b54f2b8bb69261356d0c426a473cf40
Michael Zekoff <zekoff@gmail.com>
2019-08-17 20:47:54 -0500

    Implement DB updates for completed readings

--- dbhelper.py
+++ dbhelper.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import logging
 import psycopg2
 
 DATABASE_URL = os.environ['DATABASE_URL']
@@ -12,8 +13,39 @@ ORDER BY original_row ASC
 LIMIT 1
 """
 
+UPDATE_READING_STATEMENT = """
+UPDATE Readings
+SET time_min=%(time_spent)s
+WHERE id=%(id)s
+"""
 
-def get_next_reading() -> str:
+def complete_reading(time_spent: int):
+    id, _, _ = get_next_reading()
+    cursor = psycopg2.connect(DATABASE_URL, sslmode='require').cursor()
+    success = False
+    try:
+        cursor.execute(UPDATE_READING_STATEMENT, {
+            'id': id,
+            'time_spent': time_spent
+        })
+        success = True
+    except Exception as ex:
+        logging.error(ex)
+    cursor.connection.commit()
+    cursor.close()
+    cursor.connection.close()
+    if not success:
+        raise Exception('Error updating row in DB with completed reading')
+
+def get_next_reading() -> tuple:
+    """Return next unread passage from postgres
+    
+    Returns:
+        tuple -- 3-tuple containing:
+            int (id)
+            str (passage)
+            str (week)
+    """
     cursor = psycopg2.connect(DATABASE_URL, sslmode='require').cursor()
     cursor.execute(NEXT_READING_QUERY)
     result = cursor.fetchall()
@@ -21,7 +53,7 @@ def get_next_reading() -> str:
     cursor.connection.close()
     if len(result) != 1:
         return "Reading plan complete!"
-    return result[0][1]
+    return int(result[0][0]), result[0][1], result[0][3]
 
 def get_next_reading_sqlite() -> str:
     """Fetch next unread passage from the database.
--- readingtracker.py
+++ readingtracker.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from flask import Flask, render_template, request
 import dbhelper
@@ -5,12 +6,23 @@ import dbhelper
 application = Flask(__name__)  # gunicorn looks for 'application'
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
-@application.route('/')
+@application.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', passage=dbhelper.get_next_reading())
-
-@application.route('/record_reading')
-def record_reading():
-    return 'ok'
+    _, passage, week = dbhelper.get_next_reading()
+    submitted_data = request.form.get('time_spent', None)
+    if submitted_data is not None:
+        try:
+            submitted_data = int(submitted_data)
+            dbhelper.complete_reading(submitted_data)
+        except Exception as ex:
+            logger.error(ex)
+            submitted_data = "Error submitting data"
+    logger.debug(f'Form data (if submitted): {submitted_data}')
+    template_vars = {
+        'passage': passage,
+        'week': week,
+        'submitted_data': submitted_data
+    }
+    return render_template('index.html', **template_vars)
